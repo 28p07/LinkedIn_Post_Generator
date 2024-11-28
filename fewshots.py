59566c6cd513d666708cdbf84454cfd31937c7db
Puneet Sharma <sharmapuneet8126@gmail.com>
2024-11-28 17:53:55 +0530

    updated fewshots.py

--- fewshots.py
+++ fewshots.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 
 class fewshot:
-    def __init__(self,file_path = "data\processed_posts.json"):
+    def __init__(self,file_path = "data\\processed_posts.json"):
         self.df = None
         self.unique_tags = None
         self.load_posts(file_path)
